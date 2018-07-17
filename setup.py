025a6634e222077ca84be4b580cf2956cd9d1b53
dgarcia360 <dgarcia360@outlook.com>
2018-07-17 23:35:38 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-ghcontributors',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/dgarcia360/sphinxcontrib-ghcontributors',
     # download_url='http://pypi.python.org/pypi/sphinxcontrib-ghcontributors',
     license='MIT',
