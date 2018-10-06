39ea88bfc7c34e494d7567105d29be306662b9ba
premsa <38909445+premsa@users.noreply.github.com>
2018-10-06 15:11:44 +0200

    issue #5: Order by different criteria, added options and tests

--- sphinxcontrib/ghcontributors.py
+++ sphinxcontrib/ghcontributors.py
@@ -12,7 +12,8 @@ def _render_contributor(contributor):
 
     # name and url
     node_paragraph = nodes.paragraph()
-    node_url = nodes.reference(text=contributor.get('login'), refuri=contributor.get('html_url'))
+    node_url = nodes.reference(text=contributor.get(
+        'login'), refuri=contributor.get('html_url'))
     node_paragraph += node_url
 
     # number of contributions
@@ -24,9 +25,27 @@ def _render_contributor(contributor):
     return node_paragraph
 
 
-def _render_contributors_list(contributors):
+def _render_contributors_list(contributors, sort_by_contributions=True, sort_by_contributors=False):
 
-    contributors = sorted(contributors, key=lambda k: k['contributions'], reverse=True)
+    # default case:
+    # list is sorted only by the quantity of contributions
+    if sort_by_contributions and not sort_by_contributors:
+        contributors = sorted(
+            contributors, key=lambda k: k['contributions'], reverse=True)
+
+    # list is sorted only by alphabetical order
+    elif not sort_by_contributions and sort_by_contributors:
+        contributors = sorted(
+            contributors, key=lambda k: k['login'], reverse=False)
+
+    # list is first sorted by alphabetical order and then by contributions
+    elif sort_by_contributions and sort_by_contributors:
+        contributors = sorted(
+            contributors, key=lambda k: (k['login'], -int(k["contributions"])))
+
+    # other scenarios can be easily added here, if needed
+    else:
+        contributors = contributors
 
     lst = nodes.bullet_list()
 
@@ -43,7 +62,8 @@ def _get_gh_contributors(owner, repository):
     contributors = []
 
     try:
-        r = requests.get('https://api.github.com/repos/' + owner + '/' + repository + '/contributors')
+        r = requests.get('https://api.github.com/repos/' +
+                         owner + '/' + repository + '/contributors')
         if r:
             contributors = r.json()
 
--- tests/test_directive.py
+++ tests/test_directive.py
@@ -26,3 +26,32 @@ class TestContributorsDirective(object):
 
         contributors = _get_gh_contributors('dgarcia360', 'reponotfound')
         assert contributors == []
+
+    def test_render_contributors_list_sorting_by_contributions(self):
+
+        contributors = [{'login': 'dgarcia360', 'contributions': 3, 'htmlurl': 'http://#'},
+                        {'login': 'premsa', 'contributions': 2, 'htmlurl': 'http://#'},
+                        {'login': 'user', 'contributions': 1, 'htmlurl': 'http://#'}]
+        rendered_contributors_list = _render_contributors_list(contributors)
+
+        assert rendered_contributors_list.astext() == 'dgarcia360 - 3 contributions\n\npremsa - 2 contributions\n\nuser - 1 contributions'
+
+    def test_render_contributors_list_sort_by_contributor(self):
+
+        contributors = [{'login': 'dgarcia360', 'contributions': 4, 'htmlurl': 'http://#'},
+                        {'login': 'premsa', 'contributions': 5, 'htmlurl': 'http://#'},
+                        {'login': 'alpha', 'contributions': 2, 'htmlurl': 'http://#'},
+                        {'login': 'beta', 'contributions': 1, 'htmlurl': 'http://#'}]
+        rendered_contributors_list = _render_contributors_list(contributors, sort_by_contributions=False, sort_by_contributors=True)
+
+        assert rendered_contributors_list.astext() == 'alpha - 2 contributions\n\nbeta - 1 contribution\n\ndgarcia360 - 4 contributions\n\npremsa - 5 contributions'
+
+    def test_render_contributors_list_sort_by_contributor_and_contributions(self):
+
+        contributors = [{'login': 'dgarcia360', 'contributions': 4, 'htmlurl': 'http://#'},
+                        {'login': 'premsa', 'contributions': 5, 'htmlurl': 'http://#'},
+                        {'login': 'alpha1', 'contributions': 2, 'htmlurl': 'http://#'},
+                        {'login': 'alpha2', 'contributions': 1, 'htmlurl': 'http://#'}]
+        rendered_contributors_list = _render_contributors_list(contributors, sort_by_contributions=True, sort_by_contributors=True)
+
+        assert rendered_contributors_list.astext() == 'alpha1 - 2 contributions\n\nalpha2 - 1 contribution\n\ndgarcia360 - 4 contributions\n\npremsa - 5 contributions'
